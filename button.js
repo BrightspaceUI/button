2aefd2889319b109cd35da818162faf233de9ea3
Dave Lockhart <dave.lockhart@desire2learn.com>
2013-07-04 14:22:03 -0400

    Integrating "assemble" package to build up our documentation and qunit test pages. Also some initial stuff for the button control.

---
+++ button.js
@@ -0,0 +1,21 @@
+﻿$( function() { 
+	$.widget( 'vui.button', { 
+
+		options: {},
+
+		_create: function() {
+		},
+
+		_setOption: function( key, value ) {
+		}
+
+	} );
+
+	vui.addClassInitializer(
+			'vui-button',
+			function( node ) {
+				$( node ).button();
+			}
+		);
+
+} );
