d70c3e4f92d19409455a1f4e4911627bfefa8e63
Dave Lockhart <dave.lockhart@desire2learn.com>
2013-07-08 17:59:35 -0400

    Initial button JS API with "setIsEnabled", "isEnabled" and ARIA support.

--- button.js
+++ button.js
@@ -1,12 +1,59 @@
 ﻿$( function() { 
-	$.widget( 'vui.button', { 
+	$.widget( 'vui.vui_button', { 
 
 		options: {},
 
 		_create: function() {
+
+			var $node = $( this.element );
+			if( $node.is( 'a' ) ) {
+
+				$node.attr( 'role', 'button' );
+
+				if( $node.hasClass( 'vui-disabled' ) ) {
+					$node.attr( 'aria-disabled', 'true' );
+				}
+
+			}
+
 		},
 
 		_setOption: function( key, value ) {
+		},
+
+		isEnabled: function() {
+
+			var isEnabled;
+
+			var $node = $( this.element );
+			if( $node.is( 'a' ) ) {
+				isEnabled = !$node.hasClass( 'vui-disabled' );
+			} else {
+				isEnabled = $node.attr( 'disabled' ) === undefined;
+			}
+
+			return isEnabled;
+
+		},
+
+		setIsEnabled: function( isEnabled ) {
+
+			var $node = $( this.element );
+			if( $node.is( 'a' ) ) {
+
+				$node.toggleClass( 'vui-disabled', !isEnabled )
+					.attr( 'aria-disabled', isEnabled ? 'false' : 'true' );
+
+			} else {
+				if( isEnabled ) {
+					$node.removeAttr( 'disabled' );
+				} else {
+					$node.attr( 'disabled', 'disabled' );
+				}
+			}
+
+			return this;
+
 		}
 
 	} );
@@ -14,7 +61,7 @@
 	vui.addClassInitializer(
 			'vui-button',
 			function( node ) {
-				$( node ).button();
+				$( node ).vui_button();
 			}
 		);
 
