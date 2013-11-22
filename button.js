c4a7872cd5fcd7fe7c68d148b5d489c7f20406ec
osmith <owen.smith@desire2learn.com>
2013-11-21 22:38:01 -0500

    just checking for require isn't necessarily a CommonJS env. could be AMD based

--- button.js
+++ button.js
@@ -6,10 +6,16 @@
 	// we're in a CommonJS environment; otherwise we'll just fail out
 	if( vui === undefined ) {
 		if( typeof require === 'function' ) {
-			module.exports = vui = require('../../vui');
+			vui = require('../../vui');
 		} else {
 			throw new Error('load vui first');
 		}
+	}
+
+	// Export the vui object if we're in a CommonJS environment.
+	// It will already be on the window otherwise
+	if( typeof module === 'object' && typeof module.exports === 'object' ) {
+		module.exports = vui;
 	}
 
 	$.widget( 'vui.vui_button', {
