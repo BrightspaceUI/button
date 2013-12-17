bc9a100d791d426bb4552ed07e83bb15c457a0fc
Owen Smith <owen@omsmith.ca>
2013-12-17 10:56:52 -0500

    (build) package valence using browserify

--- button.js
+++ button.js
@@ -6,7 +6,7 @@
 	// we're in a CommonJS environment; otherwise we'll just fail out
 	if( vui === undefined ) {
 		if( typeof require === 'function' ) {
-			vui = require('../../vui');
+			vui = require('../../core');
 		} else {
 			throw new Error('load vui first');
 		}
@@ -17,7 +17,7 @@
 	if( typeof module === 'object' && typeof module.exports === 'object' ) {
 		module.exports = vui;
 	}
-	
+
 	var $ = vui.$;
 
 	$.widget( 'vui.vui_button', {
