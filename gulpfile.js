fbe049154f59db00188cf4f50e9a6458a4a68594
Noel Stonehouse <noel.stonehouse@D2L.com>
2014-09-22 14:17:57 -0400

    Initial commit for test gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require( 'gulp' ),
 
 
 gulp.task( 'clean', function() {
-	return vui.clean( [ 'dist/**/*', 'lib' ] );
+	return vui.clean( [ 'dist/**/*', 'lib', 'output' ] );
 } );
 
 gulp.task( 'css', [ 'less-dependencies' ], function () {
@@ -27,4 +27,12 @@ gulp.task( 'less', function () {
 
 gulp.task( 'default', [ 'clean' ], function() {
 	gulp.start( 'css', 'less' );
+} );
+
+gulp.task( 'test', function () {
+	return vui.test(
+			'test/unit/karma.conf.js',
+			'test/unit/**/*Spec.js',
+			'dist/**/*.css'
+		);
 } );
