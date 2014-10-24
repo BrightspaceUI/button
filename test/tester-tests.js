4a9b515aa855a5780c9d9eb7a5d36851570f1b08
Noel Stonehouse <noel.stonehouse@D2L.com>
2014-10-24 16:27:47 -0400

    Update button tests to handle the Linux case with an exception.

--- test/tester-tests.js
+++ test/tester-tests.js
@@ -9,7 +9,18 @@ var verifyElementDimensions = function( elemConstructor ) {
 
 	it( 'has correct styles', function() {
 		var diff = d2l.vui.differs.diffDefaultStyle( elem );
-		expect( diff ).toMatchRecordedObject( d2l.vui.records.getElementRecordPath( elem, ['disabled'] ));
+		var recordPath = d2l.vui.records.getElementRecordPath( elem, ['disabled'] );
+		var exceptions = {};
+
+		if( d2l.vui.differs.isUserAgentOS("Linux") ) {
+			exceptions = {
+				"width" : "37px",
+				"-webkit-perspective-origin" : "23px 14px",
+				"-webkit-transform-origin" : "23px 14px"
+			};
+		}
+
+		expect( diff ).toMatchRecordedObjectAt( recordPath, exceptions );
 	});
 
 	afterEach( function() {
