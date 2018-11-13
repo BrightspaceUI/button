d4a41fc9e03bb40a2864b6d9717822cd293c7793
AdmiralCoco <dawingc@hotmail.com>
2018-11-13 11:03:11 -0500

    Minor fixes to floating-buttons-helper
    - Wait for WebComponentsReady for browsers using polyfill
    - Check for elements before attempting to remove them

--- demo/floating-buttons-helper.js
+++ demo/floating-buttons-helper.js
@@ -4,40 +4,49 @@ var page_floatingButtons;
 function appendButton() {
 	var floatingButtons = document.getElementById('wc_buttons');
 
-	page_buttonCount += 1;
+	if (floatingButtons) {
+		page_buttonCount += 1;
 
-	var newButton = document.createElement('button');
-	newButton.textContent = 'Beer ' + page_buttonCount;
+		var newButton = document.createElement('button');
+		newButton.textContent = 'Beer ' + page_buttonCount;
 
-	Polymer.dom(floatingButtons).appendChild(newButton);
+		Polymer.dom(floatingButtons).appendChild(newButton);
+	}
 }
-
-document.getElementById('btn-append-floating').addEventListener('click', function() {
-	document.body.appendChild(page_floatingButtons);
-});
-document.getElementById('btn-remove-floating').addEventListener('click', function() {
-	var floatingButtons = document.getElementById('wc_buttons');
-	page_floatingButtons = floatingButtons;
-	floatingButtons.parentNode.removeChild(floatingButtons);
-});
-document.getElementById('btn-append-new').addEventListener('click', appendButton);
-document.getElementById('btn-remove-last').addEventListener('click', function() {
-	var floatingButtons = document.getElementById('wc_buttons');
-
-	Polymer.dom(floatingButtons).removeChild(
-		Polymer.dom(floatingButtons).lastChild
-	);
-});
-document.getElementById('btn-insert-content-above').addEventListener('click', function() {
-	var newContent = document.createElement('p');
-	newContent.textContent = 'I love beer!';
-	document.body.querySelector('.content').appendChild(newContent);
-});
-document.getElementById('btn-remove-content-above').addEventListener('click', function() {
-	document.body.querySelector('.content').removeChild(
-		document.body.querySelector('.content').lastChild
-	);
-});
-document.getElementById('btn-make-more').addEventListener('click', function() {
-	appendButton();
+document.addEventListener('WebComponentsReady', function() {
+	document.getElementById('btn-append-floating').addEventListener('click', function() {
+		if (page_floatingButtons) {
+			document.body.appendChild(page_floatingButtons);
+		}
+	});
+	document.getElementById('btn-remove-floating').addEventListener('click', function() {
+		var floatingButtons = document.getElementById('wc_buttons');
+		if (floatingButtons) {
+			page_floatingButtons = floatingButtons;
+			floatingButtons.parentNode.removeChild(floatingButtons);
+		}
+	});
+	document.getElementById('btn-append-new').addEventListener('click', appendButton);
+	document.getElementById('btn-remove-last').addEventListener('click', function() {
+		var floatingButtons = document.getElementById('wc_buttons');
+		if (Polymer.dom(floatingButtons).lastChild) {
+			Polymer.dom(floatingButtons).removeChild(
+				Polymer.dom(floatingButtons).lastChild
+			);
+		}
+	});
+	document.getElementById('btn-insert-content-above').addEventListener('click', function() {
+		var newContent = document.createElement('p');
+		newContent.textContent = 'I love beer!';
+		document.body.querySelector('.content').appendChild(newContent);
+	});
+	document.getElementById('btn-remove-content-above').addEventListener('click', function() {
+		var content = document.body.querySelector('.content');
+		if (content.lastChild) {
+			content.removeChild(content.lastChild);
+		}
+	});
+	document.getElementById('btn-make-more').addEventListener('click', function() {
+		appendButton();
+	});
 });
