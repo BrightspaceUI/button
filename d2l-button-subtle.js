45390c18b71e41794aa125c476a78b98623e1310
AdmiralCoco <dawingc@hotmail.com>
2018-11-15 09:44:10 -0500

    Remove some FIXME comments

--- d2l-button-subtle.js
+++ d2l-button-subtle.js
@@ -4,11 +4,6 @@ Polymer-based web component for subtle buttons
 
 @demo demo/button-subtle.html d2l-button-subtle
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
 import '../@polymer/polymer/polymer-legacy.js';
 
 import '../d2l-colors/d2l-colors.js';
